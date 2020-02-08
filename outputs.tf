58b182a10cf6db6a115013a696a534ebed8e9a77
Rafael Dutra <raffaeldutra@gmail.com>
2020-02-08 17:12:45 -0300

    More outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "instance_id" {
 output "cpu_platform" {
   value = google_compute_instance.this.*.cpu_platform
 }
+
+output "nat_ip" {
+  value = google_compute_instance.this.*.network_interface.0.access_config.0.nat_ip
+}
